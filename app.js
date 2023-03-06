5c55cba22f92aa2b17d8eb7fca5c27642a7723ce
paslavsky9roman <46484914+paslavskyi9roman@users.noreply.github.com>
2023-03-06 18:03:55 +0200

    adding a ErrorHandler

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 
 const dotenv = require('dotenv');
 const connectDatabase = require('./config/database');
+const errorMiddleware = require('./middleware/error');
 
 dotenv.config({ path: './config/config.env' });
 
@@ -13,6 +14,8 @@ app.use(express.json());
 const jobs = require('./routes/jobs.routes');
 
 app.use('/api/v1', jobs);
+
+app.use(errorMiddleware);
 
 const PORT = process.env.PORT;
 
--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,6 +1,7 @@
 const Job = require('../models/job.model');
 
 const geoCoder = require('../utils/geocoder');
+const ErrorHandler = require('../utils/errorHandler');
 
 exports.getJobs = async (req, res, next) => {
   const jobs = await Job.find();
@@ -43,10 +44,7 @@ exports.updateJob = async (req, res, next) => {
   let job = await Job.findById(req.params.id);
 
   if (!job) {
-    res.status(404).json({
-      success: false,
-      message: 'Job not found',
-    });
+    return next(new ErrorHandler('Job not Found', 404));
   }
 
   job = await Job.findByIdAndUpdate(req.params.id, req.body, {
