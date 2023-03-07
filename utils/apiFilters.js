ece4f96d0515e88218b3f9067f7b73d046069548
paslavsky9roman <46484914+paslavskyi9roman@users.noreply.github.com>
2023-03-07 15:38:25 +0200

    adding limit, pagination and search queries

--- utils/apiFilters.js
+++ utils/apiFilters.js
@@ -7,10 +7,8 @@ class APIFilters {
   filter() {
     const queryCopy = { ...this.queryStr };
 
-    const removeFields = ['sort'];
-    removeFields.forEach((el) => {
-      delete queryCopy[el];
-    });
+    const removeFields = ['sort', 'fields', 'q', 'limit', 'page'];
+    removeFields.forEach((el) => delete queryCopy[el]);
 
     let queryStr = JSON.stringify(queryCopy);
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `$${match}`);
@@ -26,6 +24,37 @@ class APIFilters {
     } else {
       this.query = this.query.sort('-postingDate');
     }
+
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryStr.fields) {
+      const fields = this.queryStr.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+
+    return this;
+  }
+
+  searchByQuery() {
+    if (this.queryStr.q) {
+      const qu = this.queryStr.q.split('-').join(' ');
+      this.query = this.query.find({ $text: { $search: '"' + qu + '"' } });
+    }
+
+    return this;
+  }
+
+  pagination() {
+    const page = parseInt(this.queryStr.page, 10) || 1;
+    const limit = parseInt(this.queryStr.limit, 10) || 10;
+    const skipResults = (page - 1) * limit;
+
+    this.query = this.query.skip(skipResults).limit(limit);
+
     return this;
   }
 }
