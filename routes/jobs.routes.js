007839cec75257a471e3268fa21812f82ee7e06d
paslavsky9roman <46484914+paslavskyi9roman@users.noreply.github.com>
2023-03-08 18:29:07 +0200

    protecting the roles route

--- routes/jobs.routes.js
+++ routes/jobs.routes.js
@@ -3,13 +3,13 @@ const router = express.Router();
 
 const { getJobs, newJob, getJobsInRadius, updateJob, deleteJob, getJob, jobStats } = require('../controllers/jobsController');
 
-const { isAuthenticatedUser } = require('../middleware/auth');
+const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
 
 router.route('/jobs').get(getJobs);
 router.route('/job/:id/:slug').get(getJob);
 router.route('/stats/:topic').get(jobStats);
 router.route('/jobs/:zipcode/:distance').get(getJobsInRadius);
-router.route('/job/new').post(isAuthenticatedUser, newJob);
+router.route('/job/new').post(isAuthenticatedUser, authorizeRoles('employer', 'admin'), newJob);
 router.route('/job/:id').put(isAuthenticatedUser, updateJob);
 router.route('/job/:id').delete(isAuthenticatedUser, deleteJob);
 
